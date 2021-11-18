ce13d9d9fd8eaa9658e3ebc236542656e75a37ac
Walter Schreppers <schreppers@gmail.com>
2021-11-18 16:10:46 +0100

    rename folder into bulma_customization. add automated script to re-compile and push changed core.css and overrides into flask app.

---
+++ bulma_customization/push_to_flask.sh
@@ -0,0 +1,9 @@
+echo "re-compiling bulma components"
+./compile.sh
+
+echo "updating flask core css and overrides..."
+cp -r dist/css/mystyles.css ../app/static/bulma/core.css
+cp src/overrides.css ../app/static/bulma/overrides.css
+
+echo "all done."
+
