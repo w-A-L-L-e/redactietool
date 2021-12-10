b1fd6fb34b58c835109594197fdcba9e6e015440
Miel Vander Sande <miel.vandersande@meemoo.be>
2021-12-10 16:37:57 +0100

    Deletes query files and moves them in python

--- app/services/suggest/Suggest.py
+++ app/services/suggest/Suggest.py
@@ -5,8 +5,177 @@ from SPARQLWrapper import JSON, POST, SPARQLWrapper2
 
 OND_NS = "https://data.meemoo.be/terms/ond/"
 
+GET_LIST_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+
+    BIND(URI('{scheme}') AS ?scheme)
+
+    ?id a skos:Concept;
+    skos:prefLabel ?label;
+    skos:inScheme ?scheme.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+}}
+"""
+
+GET_COLLECTION_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    BIND(URI('{collection}') AS ?collection)
+    ?collection skos:member ?id.
+
+    ?id a skos:Concept;
+    skos:prefLabel ?label.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+}}
+"""
+
+GET_CHILDREN_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    BIND(URI('{concept}') AS ?concept)
+    ?concept skos:narrower ?id.
+
+    ?id a skos:Concept;
+    skos:prefLabel ?label.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+}}
+"""
+
+SUGGEST_BY_LABELS_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    ?id a skos:Concept;
+    skos:prefLabel ?label;
+    skos:inScheme ond:vak;
+    skos:related ?thema, ?graad.
+
+    ?thema skos:inScheme ond:themas.
+    ond:graad skos:member ?graad.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+    
+    ?thema skos:prefLabel ?thema_label.
+    ?graad skos:prefLabel ?graad_label.
+
+    FILTER (
+        STR(?thema_label) IN ({themas}) && 
+        STR(?graad_label) IN ({graden}) )
+}}
+"""
+
+SUGGEST_BY_IDS_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    ?id a skos:Concept;
+    skos:prefLabel ?label;
+    skos:inScheme ond:vak;
+    skos:related ?thema, ?graad.
+    
+    ?thema skos:inScheme ond:themas.
+    ond:graad skos:member ?graad.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+
+    FILTER (
+        ?thema IN ({themas}) && 
+        ?graad IN ({graden}) )
+}}
+"""
+
+GET_CONCEPT_BY_ID_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    BIND(URI('{concept}') AS ?id)
+    ?id a skos:Concept;
+    skos:prefLabel ?label.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+}}
+"""
+
+GET_CANDIDATES_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{
+    ?id a skos:Concept;
+    skos:prefLabel ?label;
+    skos:inScheme ond:vak;
+    skos:related ?thema, ?graad.
+    
+    ?thema skos:inScheme ond:themas.
+    ond:graad skos:member ?graad.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+
+    FILTER (
+        ?thema IN ({themas}) || 
+        ?graad IN ({graden}) )
+
+}}
+"""
+
+GET_RELATED_VAK_QUERY = """
+PREFIX ond: <https://data.meemoo.be/terms/ond/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT DISTINCT ?id ?label ?definition
+WHERE {{  
+    ?id a skos:Concept;
+    skos:prefLabel ?label;
+    skos:inScheme ond:vak;
+    skos:related ?concept.
+
+    OPTIONAL {{
+        ?id skos:definition ?definition .
+    }}
+
+    FILTER (?concept IN ({concepts})) 
+
+}}
+"""
+
+
 def join_ids(ids):
     return ", ".join("<" + str(id) + ">" for id in ids)
+
 
 def read_query(file_path):
     # check if file is present
@@ -32,18 +201,6 @@ class Suggest:
         self.sparql.setCredentials(user, password)
         self.sparql.setReturnFormat(JSON)
 
-        query_dir = os.getcwd() + "/queries/"
-        self.GET_LIST_QUERY = read_query(query_dir + "get_conceptscheme.sparql")
-        self.GET_COLLECTION_QUERY = read_query(query_dir + "get_collection.sparql")
-        self.GET_CHILDREN_QUERY = read_query(query_dir + "get_narrower.sparql")
-        self.SUGGEST_BY_LABELS_QUERY = read_query(
-            query_dir + "suggest_by_labels.sparql"
-        )
-        self.SUGGEST_BY_IDS_QUERY = read_query(query_dir + "suggest.sparql")
-        self.GET_CONCEPT_BY_ID_QUERY = read_query(query_dir + "get_concept.sparql")
-        self.GET_CANDIDATES_QUERY = read_query(query_dir + "candidates.sparql")
-        self.GET_RELATED_VAK_QUERY = read_query(query_dir + "get_related_vak.sparql")
-
     def __exec_query(self, query: str, **kwargs):
         formatted = query.format(**kwargs)
         self.sparql.setQuery(formatted)
@@ -57,25 +214,25 @@ class Suggest:
     def get_list(self, scheme: str):
         """Get thesaurus concepts by scheme id."""
 
-        for res in self.__exec_query(self.GET_LIST_QUERY, scheme=scheme):
+        for res in self.__exec_query(GET_LIST_QUERY, scheme=scheme):
             yield res
 
     def get_concept(self, concept: str):
         """Get thesaurus concept by id."""
 
-        for res in self.__exec_query(self.GET_CONCEPT_BY_ID_QUERY, concept=concept):
+        for res in self.__exec_query(GET_CONCEPT_BY_ID_QUERY, concept=concept):
             yield res
 
     def get_collection(self, collection: str):
         """Get a collection members by collection id."""
 
-        for res in self.__exec_query(self.GET_COLLECTION_QUERY, collection=collection):
+        for res in self.__exec_query(GET_COLLECTION_QUERY, collection=collection):
             yield res
 
     def get_children(self, concept: str):
         """Get the children of a concept."""
 
-        for res in self.__exec_query(self.GET_CHILDREN_QUERY, concept=concept):
+        for res in self.__exec_query(GET_CHILDREN_QUERY, concept=concept):
             yield res
 
     def get_vakken(self):
@@ -109,7 +266,7 @@ class Suggest:
         graden = join_ids(graad)
 
         for res in self.__exec_query(
-            self.SUGGEST_BY_IDS_QUERY, themas=themas, graden=graden
+            SUGGEST_BY_IDS_QUERY, themas=themas, graden=graden
         ):
             yield res
 
@@ -119,9 +276,9 @@ class Suggest:
         themas = ", ".join('"' + str(t) + '"' for t in thema)
         graden = ", ".join('"' + str(g) + '"' for g in graad)
 
-        print("running qry=", self.SUGGEST_BY_LABELS_QUERY)
+        print("running qry=", SUGGEST_BY_LABELS_QUERY)
         for res in self.__exec_query(
-            self.SUGGEST_BY_LABELS_QUERY, themas=themas, graden=graden
+            SUGGEST_BY_LABELS_QUERY, themas=themas, graden=graden
         ):
             yield res
 
@@ -132,7 +289,7 @@ class Suggest:
         graden = join_ids(graad)
 
         for res in self.__exec_query(
-            self.GET_CANDIDATES_QUERY, themas=themas, graden=graden
+            GET_CANDIDATES_QUERY, themas=themas, graden=graden
         ):
             yield res
 
@@ -141,5 +298,5 @@ class Suggest:
 
         concepts = join_ids(concept)
 
-        for res in self.__exec_query(self.GET_RELATED_VAK_QUERY, concepts=concepts):
+        for res in self.__exec_query(GET_RELATED_VAK_QUERY, concepts=concepts):
             yield res
