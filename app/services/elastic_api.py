c5aafa2813fec251594cc3cf3094919f4777f84e
Walter Schreppers <schreppers@gmail.com>
2022-04-01 15:36:34 +0200

    fix lint errors. keyword searching now filters duplicates which gives better search results

--- app/services/elastic_api.py
+++ app/services/elastic_api.py
@@ -44,7 +44,7 @@ class ElasticApi:
                     "prefix": qry,
                     "completion": {
                         "field": "lom_keywords.suggest",
-                        "skip_duplicates": True 
+                        "skip_duplicates": True
                     }
                 }
             }
